0f78553d0b7a778f89b34c6b4d313155ee51fcbf
manolo <viprestal@hotmail.com>
2021-03-10 19:40:19 +0100

    video 04

---
+++ tailwind/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="{{asset('css/app.css')}}" rel="stylesheet">
+  <title>Tailwind</title>
+  <style>
+    
+  </style>
+</head>
+<body>
+ <div class="container">
+
+    <h1 class="text-xl"></h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, eius voluptas quam minus adipisci neque officiis in saepe quae labore. Velit molestiae quas quasi, dolore odit sapiente assumenda architecto unde!</p>
+
+    <ul class="list-decimal list-inside">
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati sequi dolor voluptatibus laboriosam perferendis doloremque consectetur labore voluptates voluptatem dolore! Enim quisquam voluptates nulla. Commodi ex iusto deserunt odit incidunt.</li>
+        <li>Deleniti voluptatibus quasi maiores nobis, id dolores laboriosam repellendus atque at accusantium optio excepturi veritatis libero sed. Possimus saepe odit molestias aut nulla laudantium expedita perspiciatis alias. Mollitia, ad eaque?</li>
+        <li>Voluptates, expedita aliquam? Fugit alias nesciunt ad soluta ratione quis molestiae amet eum nobis dolor blanditiis fuga neque odio eaque assumenda quos dolore veniam sit, nostrum magnam distinctio, aperiam et!</li>
+        <li>Error velit provident earum impedit odit. Accusantium odit culpa dolorem reprehenderit quia et, cum nisi facilis excepturi, explicabo fugiat. Iusto numquam distinctio quaerat optio a laudantium laboriosam velit. Ipsam, nostrum?</li>
+        <li>Doloribus et recusandae impedit earum temporibus molestiae, quos libero quaerat aperiam est animi. Vel maxime rerum porro? Eaque a sunt nulla architecto dolorem, dignissimos modi, laudantium libero, voluptate obcaecati quasi.</li>
+    </ul>
+</div>
+
+</body>
+</html>

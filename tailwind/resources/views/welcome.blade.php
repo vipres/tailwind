acc17b5d1bbf5ca6b2cda788b6d7d60f643c441c
manolo <viprestal@hotmail.com>
2021-03-13 10:45:55 +0100

    emezando 6

--- tailwind/resources/views/welcome.blade.php
+++ tailwind/resources/views/welcome.blade.php
@@ -6,24 +6,26 @@
   <link href="{{asset('css/app.css')}}" rel="stylesheet">
   <title>Tailwind</title>
   <style>
-    
+
   </style>
 </head>
 <body>
  <div class="container">
 
-    <h1 class="text-xl"></h1>
+    <h1 class="uppercase text-xl font-robo font-bold text-left md:text-center lg:text-right">Este es el titulo</h1>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, eius voluptas quam minus adipisci neque officiis in saepe quae labore. Velit molestiae quas quasi, dolore odit sapiente assumenda architecto unde!</p>
+    <p class="text-red-700 hover:text-opacity-75">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, eius voluptas quam minus adipisci neque officiis in saepe quae labore. Velit molestiae quas quasi, dolore odit sapiente assumenda architecto unde!</p>
 
-    <ul class="list-decimal list-inside">
-        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati sequi dolor voluptatibus laboriosam perferendis doloremque consectetur labore voluptates voluptatem dolore! Enim quisquam voluptates nulla. Commodi ex iusto deserunt odit incidunt.</li>
+    <ul class="list-decimal md:list-disc list-inside">
+        <li class="line-through">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati sequi dolor voluptatibus laboriosam perferendis doloremque consectetur labore voluptates voluptatem dolore! Enim quisquam voluptates nulla. Commodi ex iusto deserunt odit incidunt.</li>
         <li>Deleniti voluptatibus quasi maiores nobis, id dolores laboriosam repellendus atque at accusantium optio excepturi veritatis libero sed. Possimus saepe odit molestias aut nulla laudantium expedita perspiciatis alias. Mollitia, ad eaque?</li>
         <li>Voluptates, expedita aliquam? Fugit alias nesciunt ad soluta ratione quis molestiae amet eum nobis dolor blanditiis fuga neque odio eaque assumenda quos dolore veniam sit, nostrum magnam distinctio, aperiam et!</li>
         <li>Error velit provident earum impedit odit. Accusantium odit culpa dolorem reprehenderit quia et, cum nisi facilis excepturi, explicabo fugiat. Iusto numquam distinctio quaerat optio a laudantium laboriosam velit. Ipsam, nostrum?</li>
-        <li>Doloribus et recusandae impedit earum temporibus molestiae, quos libero quaerat aperiam est animi. Vel maxime rerum porro? Eaque a sunt nulla architecto dolorem, dignissimos modi, laudantium libero, voluptate obcaecati quasi.</li>
+        <li class="lowercase">Doloribus et recusandae impedit earum temporibus molestiae, quos libero quaerat aperiam est animi. Vel maxime rerum porro? Eaque a sunt nulla architecto dolorem, dignissimos modi, laudantium libero, voluptate obcaecati quasi.</li>
     </ul>
 </div>
+<span class="capitalize">manuel cabrera vivas</span>
+Video 6
 
 </body>
 </html>
